175ebe91d0a4e3fc1296e2b0f15a757e93918825
zechsoft <zechsoft.it@gmail.com>
2026-05-19 13:33:53 +0530

    ,M,M

--- backend/server.js
+++ backend/server.js
@@ -52,10 +52,13 @@ app.use(helmet());
 app.disable('x-powered-by');
 
 // Secure CORS to only allow specific frontend origin in production
-const allowedOrigins = process.env.FRONTEND_URL ? [process.env.FRONTEND_URL] : ['http://localhost:5173', 'http://localhost:5174'];
+const frontendUrl = process.env.FRONTEND_URL ? process.env.FRONTEND_URL.replace(/\/$/, '') : null;
+const allowedOrigins = frontendUrl ? [frontendUrl] : ['http://localhost:5173', 'http://localhost:5174'];
+
 app.use(cors({
   origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1 || process.env.NODE_ENV !== 'production') {
+    const originSafe = origin ? origin.replace(/\/$/, '') : '';
+    if (!origin || allowedOrigins.includes(originSafe) || process.env.NODE_ENV !== 'production') {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
